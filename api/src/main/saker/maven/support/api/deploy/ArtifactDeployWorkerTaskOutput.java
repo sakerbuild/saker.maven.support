b1b9a6cb506140e1e1627b9d245de5211003e6b5
sipkab <10866741+Sipkab@users.noreply.github.com>
2020-01-10 19:46:45 +0100

    Move ArtifactDeployWorkerTaskOutput as it was in the wrong package

--- api/src/main/saker/maven/support/api/deploy/ArtifactDeployWorkerTaskOutput.java
+++ api/src/main/saker/maven/support/api/deploy/ArtifactDeployWorkerTaskOutput.java
@@ -13,7 +13,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package saker.maven.support.api;
+package saker.maven.support.api.deploy;
+
+import saker.maven.support.api.ArtifactCoordinates;
 
 public interface ArtifactDeployWorkerTaskOutput {
 	public ArtifactCoordinates getCoordinates();
